cfbfeef6cc245c7a51a450a419c66f1d69e5169c
zee-ham-su <hsufiian@yahoo.com>
2024-01-30 17:53:49 +0000

    html

--- startup/blog/views.py
+++ startup/blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 30, 2024'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 31, 2024'
+    }
+]
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
