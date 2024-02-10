3c24e92db29494160fc819295346e7adc8857583
zee-ham-su <hsufiian@yahoo.com>
2024-02-10 20:33:13 +0000

    Add Post model to blog app

--- startup/blog/models.py
+++ startup/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model): 
+    title = models.CharField(max_length=100) # max_length is a required field
+    content = models.TextField()  # TextField is a required field
+    date_posted = models.DateTimeField(default=timezone.now) # auto_now_add is a required field
+    author = models.CharField(max_length=100) # max_length is a required field
+
+    def __str__(self): 
+        return self.title # This will return the title of the post in the admin panel
