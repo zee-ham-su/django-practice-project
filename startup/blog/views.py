0d065c3433810f158c363e24f3dcb9687d60e01c
zee-ham-su <hsufiian@yahoo.com>
2024-02-11 10:43:47 +0000

    Remove dummy data from views.py

--- startup/blog/views.py
+++ startup/blog/views.py
@@ -1,20 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 30, 2024'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 31, 2024'
-    }
-]
+
 # Create your views here.
 def home(request):
     context = {
