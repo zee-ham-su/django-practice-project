15577cf72ad6362eec93ecf332bb2d16b0dc76c5
zee-ham-su <hsufiian@yahoo.com>
2024-02-11 10:26:26 +0000

    Update blog views to use Post model

--- startup/blog/views.py
+++ startup/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 
 posts = [
     {
@@ -17,7 +18,7 @@ posts = [
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
