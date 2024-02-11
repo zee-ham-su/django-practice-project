082b3816f65e61409bd07a7de72e309935ae8677
zee-ham-su <hsufiian@yahoo.com>
2024-02-11 09:44:20 +0000

    Refactor Post model in blog app

--- startup/blog/models.py
+++ startup/blog/models.py
@@ -4,10 +4,8 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model): 
-    title = models.CharField(max_length=100) # max_length is a required field
-    content = models.TextField()  # TextField is a required field
-    date_posted = models.DateTimeField(default=timezone.now) # auto_now_add is a required field
-    author = models.CharField(max_length=100) # max_length is a required field
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self): 
-        return self.title # This will return the title of the post in the admin panel
